112722d1fc340817f332ce57787f271a7235e1e8
amritpoudel11@gmail.com <amritpoudel11@gmail.com>
2020-03-06 22:50:56 +0100

    added initial book model

---
+++ src/main/java/dev/amrit/bookdb/model/Book.java
@@ -0,0 +1,50 @@
+package dev.amrit.bookdb.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    @Column
+    private String title;
+    @Column
+    private String author;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
